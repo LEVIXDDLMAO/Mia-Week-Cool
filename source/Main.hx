e581bd21792e0c7622303958bdf94c3d74d60d81
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-10-02 01:53:03 -0600

    nhbgfghjkjhgbnmjhgbn

--- source/Main.hx
+++ source/Main.hx
@@ -17,7 +17,6 @@ class Main extends Sprite
 
 	public static var video:Bool = !Sys.args().contains("-novid");
 	public static var preload:Bool = !Sys.args().contains("-nopreload");
-	public static var preload:Bool = true;
 
 	public function new()
 	{
